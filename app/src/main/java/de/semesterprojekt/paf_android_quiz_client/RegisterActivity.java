b9bb3c4bd2d2b451b732a7f1ab4ee3665667bd57
Felicitas Yeboah <felicitas.yeboah@stud.th-luebeck.de>
2021-12-12 15:47:30 +0100

    Added login link and validate inputs, added comments

--- app/src/main/java/de/semesterprojekt/paf_android_quiz_client/RegisterActivity.java
+++ app/src/main/java/de/semesterprojekt/paf_android_quiz_client/RegisterActivity.java
@@ -4,18 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import de.semesterprojekt.paf_android_quiz_client.model.AbstractRestServiceListener;
-import de.semesterprojekt.paf_android_quiz_client.model.Model;
-import de.semesterprojekt.paf_android_quiz_client.model.User;
+import de.semesterprojekt.paf_android_quiz_client.model.RestServiceListener;
+import de.semesterprojekt.paf_android_quiz_client.model.RestServiceSingleton;
 
 public class RegisterActivity extends AppCompatActivity {
     Button btn_register;
     EditText et_username, et_password;
+    TextView lnk_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
         btn_register = findViewById(R.id.btn_register);
         et_username = findViewById(R.id.et_username);
         et_password = findViewById(R.id.et_password);
+        lnk_login = findViewById(R.id.tv_loginLink);
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,16 +35,37 @@ public class RegisterActivity extends AppCompatActivity {
 
                 String username = et_username.getText().toString();
                 String password = et_password.getText().toString();
-                final Model model = Model.getInstance(RegisterActivity.this.getApplication());
-                model.register(username, password, new AbstractRestServiceListener() {
-                    @Override
+
+                //validating inputs
+                if (TextUtils.isEmpty(username)) {
+                    et_username.setError("Please enter your username");
+                    et_username.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    et_password.setError("Please enter your password");
+                    et_password.requestFocus();
+                    return;
+                }
+                //Get singleton instance
+                final RestServiceSingleton restServiceSingleton = RestServiceSingleton.getInstance(RegisterActivity.this.getApplication());
+                restServiceSingleton.register(username, password, new RestServiceListener() {
+
                     public void onRegister(String username) {
                         super.onRegister(username);
-                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                         startActivity(intent);
                         Toast.makeText(RegisterActivity.this, "User " + username + " successfully registered", Toast.LENGTH_SHORT).show();
                     }
                 });
+            }
+        });
+
+        // This happens, when clicking on the login link
+        lnk_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
     }
