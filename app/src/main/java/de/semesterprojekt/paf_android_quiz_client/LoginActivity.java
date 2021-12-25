939322257a2c076e1b587c0440ce6162ec9019cb
Felicitas Yeboah <felicitas.yeboah@stud.th-luebeck.de>
2021-12-25 20:38:47 +0100

    refactoring and commenting

--- app/src/main/java/de/semesterprojekt/paf_android_quiz_client/LoginActivity.java
+++ app/src/main/java/de/semesterprojekt/paf_android_quiz_client/LoginActivity.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,7 +18,6 @@ import de.semesterprojekt.paf_android_quiz_client.model.restservice.RestServiceS
 import de.semesterprojekt.paf_android_quiz_client.model.User;
 
 public class LoginActivity extends AppCompatActivity {
-
     Button btn_login;
     TextView lnk_register;
     EditText et_username, et_password;
@@ -28,68 +26,102 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViews();
+        setOnClickListeners();
 
+    }
 
-        // assigning values to button, textView and editText on layout
+    /**
+     * assigning values to button, textView and editText on layout
+     */
+    protected void initViews() {
         btn_login = findViewById(R.id.btn_login);
         lnk_register = findViewById(R.id.tv_registerLink);
         et_username = findViewById(R.id.et_username);
         et_password = findViewById(R.id.et_password);
+    }
 
-        // This happens, when clicking on the login button
-        btn_login.setOnClickListener(new View.OnClickListener() {
+    /**
+     * Set onClick Listeners to the buttons
+     */
+    protected void setOnClickListeners() {
+        btn_login.setOnClickListener(view -> onLoginButtonClick());
+        lnk_register.setOnClickListener(view -> onRegisterButtonClick());
+    }
+
+    /**
+     * This happens, when clicking on the login button
+     */
+    protected void onLoginButtonClick() {
+        String username = et_username.getText().toString();
+        String password = et_password.getText().toString();
+        validateInputs(username, password);
+        loginToRestService(username, password);
+    }
+
+    /**
+     * This happens, when clicking on the register link
+     */
+    protected void onRegisterButtonClick() {
+        startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+    }
+
+    /**
+     * validate inputs
+     *
+     * @param username username from inputfield
+     * @param password password from inputfield
+     */
+    protected void validateInputs(String username, String password) {
+        if (TextUtils.isEmpty(username)) {
+            et_username.setError("Please enter your username");
+            et_username.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            et_password.setError("Please enter your password");
+            et_password.requestFocus();
+        }
+    }
+
+    /**
+     * Logs the user in to the restServer
+     *
+     * @param username username
+     * @param password password
+     */
+    protected void loginToRestService(String username, String password) {
+        RestServiceSingleton restServiceSingleton = RestServiceSingleton.getInstance(LoginActivity.this.getApplication());
+        restServiceSingleton.login(username, password, new RestServiceListener() {
             @Override
-            public void onClick(View view) {
-
-                String username = et_username.getText().toString();
-                String password = et_password.getText().toString();
-
-                //validating inputs
-                if (TextUtils.isEmpty(username)) {
-                    et_username.setError("Please enter your username");
-                    et_username.requestFocus();
-                    return;
+            public void onLogin(User user) {
+                super.onLogin(user);
+                if (user != null) {
+                    storeUserDataToSharedPref(user);
+                    restServiceSingleton.setUser(user); //redundant
+                    goToStartmenu(user);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid login", Toast.LENGTH_LONG).show();
                 }
-
-                if (TextUtils.isEmpty(password)) {
-                    et_password.setError("Please enter your password");
-                    et_password.requestFocus();
-                    return;
-                }
-
-                final RestServiceSingleton restServiceSingleton = RestServiceSingleton.getInstance(LoginActivity.this.getApplication());
-
-                restServiceSingleton.login(username, password, new RestServiceListener() {
-                    @Override
-                    public void onLogin(User user) {
-                        super.onLogin(user);
-                        if (user != null) {
-
-                            storeUserDataToSharedPref(user);
-
-                            restServiceSingleton.setUser(user);
-                            Intent intent = new Intent(getApplicationContext(), StartmenueActivity.class);
-                            startActivity(intent);
-                            Toast.makeText(LoginActivity.this, "User " + user.getUsername() + " logged in", Toast.LENGTH_SHORT).show();
-                            Log.d("Quiz", user.toString());
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Invalid login", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-            }
-        });
-
-        // This happens, when clicking on the register link
-        lnk_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
     }
 
     /**
+     * Switch to the startmenu activity
+     *
+     * @param user
+     */
+    protected void goToStartmenu(User user) {
+        Intent intent = new Intent(getApplicationContext(), StartmenueActivity.class);
+        startActivity(intent);
+        Toast.makeText(LoginActivity.this, "User " + user.getUsername() + " logged in", Toast.LENGTH_SHORT).show();
+        Log.d("Quiz", user.toString());
+    }
+
+    /**
+     * Stores an username and usertoken in SharedPreferences
+     *
      * @param user the logged in user
      */
     public void storeUserDataToSharedPref(User user) {
@@ -104,7 +136,9 @@ public class LoginActivity extends AppCompatActivity {
         // apply for async
         editor.apply(); // or ed.commit(); for sync
     }
+
+
+    //TODO: Absicherung, dass man sich nciht doppelt einloggen kann. Was ist mit if (user!= null);
 }
 
 
-//TODO: Absicherung, dass man sich nciht doppelt einloggen kann. Was ist mit if (user!= null);
