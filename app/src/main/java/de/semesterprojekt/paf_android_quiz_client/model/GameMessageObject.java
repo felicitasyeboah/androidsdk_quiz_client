d0d5daf1240e0517a18d981e2bbc44932fccb997
Felicitas Yeboah <felicitas.yeboah@stud.th-luebeck.de>
2021-12-27 17:13:54 +0100

    changed GameMessageObject class to fit new stomp response message

--- app/src/main/java/de/semesterprojekt/paf_android_quiz_client/model/GameMessageObject.java
+++ app/src/main/java/de/semesterprojekt/paf_android_quiz_client/model/GameMessageObject.java
@@ -1,26 +1,30 @@
 package de.semesterprojekt.paf_android_quiz_client.model;
 
+import androidx.annotation.NonNull;
+
 /**
  * Creates an GameMessageobject from JSONString-Data received from the server via Websocket
  */
 public class GameMessageObject {
 
-    private int userScore;
-    private int opponentScore;
     private String category;
     private String question;
     private String answer1;
     private String answer2;
     private String answer3;
     private String answer4;
+    private int correctAnswer;
     User user;
     User opponent;
+    private int userScore;
+    private int opponentScore;
+
 
     public GameMessageObject() {
 
     }
 
-    public GameMessageObject(String category, String question, String answer1, String answer2, String answer3, String answer4, int userScore, int opponentScore, User user, User opponent) {
+    public GameMessageObject(String category, String question, String answer1, String answer2, String answer3, String answer4, int correctAnswer, User user, User opponent, int userScore, int opponentScore) {
         this.userScore = userScore;
         this.opponentScore = opponentScore;
         this.category = category;
@@ -29,42 +33,26 @@ public class GameMessageObject {
         this.answer2 = answer2;
         this.answer3 = answer3;
         this.answer4 = answer4;
+        this.correctAnswer = correctAnswer;
         this.user = user;
         this.opponent = opponent;
     }
 
-    // {"category":"Essen & Trinken","question":"Aus welchem Land kommt der Gouda?","answer":["Ghana","Niederlande","Frankreich","Luxemburg"],"userScore":0,"opponentScore":0,"user":{"userId":0,"userName":"Bernd","profileImage":null,"ready":false},"opponent":{"userId":0,"userName":"Beate","profileImage":null,"ready":false}}
+//BODY: {
+// "category":"Kultur",
+// "question":"Wer oder was ist \"Debussy\"?",
+// "answer1":"ein Theaterstück von Roger Vontobel",
+// "answer2":"ein Notenschlüssel aus Asien","answer3":"eine Gitarre aus Kolumbien",
+// "answer4":"ein französischer Komponist des Impressionismus",
+// "correctAnswer":4,
+// "user":{"userName":"alf","profileImage":"default10.png"},
+// "opponent":{"userName":"feli","profileImage":"default9.png"},
+// "userScore":0,
+// "opponentScore":0,
+// "type":"GAME_MESSAGE"}
 
 
     // Getter & Setter
-    public User getUser() {
-        return user;
-    }
-
-    public User getOpponent() {
-        return opponent;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public int getUserScore() {
-        return userScore;
-    }
-
-    public void setUserScore(int userScore) {
-        this.userScore = userScore;
-    }
-
-    public int getOpponentScore() {
-        return opponentScore;
-    }
-
-    public void setOpponentScore(int opponentScore) {
-        this.opponentScore = opponentScore;
-    }
-
     public String getCategory() {
         return category;
     }
@@ -111,5 +99,60 @@ public class GameMessageObject {
 
     public void setAnswer4(String answer4) {
         this.answer4 = answer4;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getOpponent() {
+        return opponent;
+    }
+
+    public void setOpponent(User opponent) {
+        this.opponent = opponent;
+    }
+
+    public int getUserScore() {
+        return userScore;
+    }
+
+    public void setUserScore(int userScore) {
+        this.userScore = userScore;
+    }
+
+    public int getOpponentScore() {
+        return opponentScore;
+    }
+
+    public void setOpponentScore(int opponentScore) {
+        this.opponentScore = opponentScore;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "category " + this.category + "\n" +
+                "question " + this.question + "\n" +
+                "answer1 " + this.answer1 + "\n" +
+                "answer2 " + this.answer2 + "\n" +
+                "answer4 " + this.answer2 + "\n" +
+                "correctAnswer " + this.correctAnswer + "\n" +
+                "user " + this.user.toString() + "\n" +
+                "opponent " + this.opponent.toString() + "\n" +
+                "userScore " + this.userScore + "\n" +
+                "opponentScore " + this.opponentScore;
     }
 }
