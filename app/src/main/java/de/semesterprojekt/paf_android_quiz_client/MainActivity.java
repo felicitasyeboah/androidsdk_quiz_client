3bb102aa71b14cac16a94f95c669970d56d58c37
Felicitas Yeboah <felicitas.yeboah@stud.th-luebeck.de>
2021-12-28 17:30:59 +0100

    added comment

--- app/src/main/java/de/semesterprojekt/paf_android_quiz_client/MainActivity.java
+++ app/src/main/java/de/semesterprojekt/paf_android_quiz_client/MainActivity.java
@@ -6,21 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
+/**
+ * Controlls the main View
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     /** Called when the user taps the Login button */
     public void gotToLogin(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
-
     }
 
     /**
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     public void gotToRegister(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
-
     }
 }
 //TODO: jwtoken in SharedPreferences stays as long as user clicks on logout or terminates the Session/closes the Application
