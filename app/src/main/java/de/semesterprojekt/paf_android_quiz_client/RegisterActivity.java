0d6be989057650bbee46b44fad41398ffa93da96
Felicitas Yeboah <felicitas.yeboah@stud.th-luebeck.de>
2021-12-28 17:39:10 +0100

    refactoring + added comments

--- app/src/main/java/de/semesterprojekt/paf_android_quiz_client/RegisterActivity.java
+++ app/src/main/java/de/semesterprojekt/paf_android_quiz_client/RegisterActivity.java
@@ -14,16 +14,20 @@ import android.widget.Toast;
 import de.semesterprojekt.paf_android_quiz_client.model.restservice.RestServiceListener;
 import de.semesterprojekt.paf_android_quiz_client.model.restservice.RestServiceSingleton;
 
+/**
+ * Controls Register View / Layout
+ */
 public class RegisterActivity extends AppCompatActivity {
     Button btn_register;
     EditText et_username, et_password;
     TextView lnk_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        // assigning values to button and editText on layout
+        // assigning values from button and editText on layout
         btn_register = findViewById(R.id.btn_register);
         et_username = findViewById(R.id.et_username);
         et_password = findViewById(R.id.et_password);
@@ -33,22 +37,17 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                // Get username and password from Inputs
                 String username = et_username.getText().toString();
                 String password = et_password.getText().toString();
 
-                //validating inputs
-                if (TextUtils.isEmpty(username)) {
-                    et_username.setError("Please enter your username");
-                    et_username.requestFocus();
-                    return;
-                }
-                if (TextUtils.isEmpty(password)) {
-                    et_password.setError("Please enter your password");
-                    et_password.requestFocus();
-                    return;
-                }
-                //Get singleton instance
+                // Validate uername and password from Inputs
+                validateInputs(username, password);
+
+                // Get singleton instance to RestAPI-Server
                 final RestServiceSingleton restServiceSingleton = RestServiceSingleton.getInstance(RegisterActivity.this.getApplication());
+
+                // Register user in Database
                 restServiceSingleton.register(username, password, new RestServiceListener() {
 
                     public void onRegister(String username) {
@@ -68,5 +67,25 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
+    }
+
+    /**
+     * Validates user inputs
+     *
+     * @param username username
+     * @param password password
+     */
+    public void validateInputs(String username, String password) {
+        //validating inputs
+        if (TextUtils.isEmpty(username)) {
+            et_username.setError("Please enter your username");
+            et_username.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            et_password.setError("Please enter your password");
+            et_password.requestFocus();
+            return;
+        }
     }
 }
