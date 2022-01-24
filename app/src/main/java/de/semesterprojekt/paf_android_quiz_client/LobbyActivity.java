aed80afa5d91f98334cd065f01739f0be555f65c
Felicitas Yeboah <felicitas.yeboah@stud.th-luebeck.de>
2022-01-24 16:42:38 +0100

    1. added hamburger main-menu in the app bar in the upper right corner to navigate 2. optimized layout

--- app/src/main/java/de/semesterprojekt/paf_android_quiz_client/LobbyActivity.java
+++ app/src/main/java/de/semesterprojekt/paf_android_quiz_client/LobbyActivity.java
@@ -1,36 +1,51 @@
 package de.semesterprojekt.paf_android_quiz_client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import de.semesterprojekt.paf_android_quiz_client.model.restservice.RestServiceSingleton;
+import de.semesterprojekt.paf_android_quiz_client.model.SessionManager;
 
 
 /**
- * Controls StartmenuView / Layout
+ * Controls LobbyView / Layout
  */
-public class StartmenueActivity extends AppCompatActivity {
+public class LobbyActivity extends AppCompatActivity {
 
     TextView tv_username;
+    LinearLayout ll_profile;
     ImageView iv_user_icon;
-    Button btn_startGame, btn_getHighscore, btn_profil, btn_logout;
+    Button btn_startGame, btn_getHighscore;
+    SessionManager sessionManager;
     SharedPreferences sharedPreferences;
     String userToken;
     String userName;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        sessionManager = new SessionManager(getApplicationContext());
+        sessionManager.checkLogin();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_startmenue);
+        setContentView(R.layout.activity_lobby);
 
         // Open SharedPref file
         sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.pref_file_key), Context.MODE_PRIVATE);
@@ -53,10 +68,11 @@ public class StartmenueActivity extends AppCompatActivity {
                 getHighscore(view);
             }
         });
-        btn_logout.setOnClickListener(new View.OnClickListener() {
+
+        ll_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                logout(view);
+                getProfile();
             }
         });
 
@@ -64,17 +80,50 @@ public class StartmenueActivity extends AppCompatActivity {
     //TODO: make Activity and LayoutView for Profil
 
     /**
+     * Displays Menu in the upper right corner in App-/Toolbar
+     * @param menu main menu
+     * @return boolean
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    /**
+     *
+     * @param item menu item
+     * @return boolean
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+            if(itemId == R.id.profile) {
+                getProfile();
+            }
+            else if (itemId == R.id.logout) {
+                logout();
+            }
+            else if (itemId == R.id.quit) {
+                //TODO: APP schließen!! Wenn nicht zu loesen, dann exit aus menue loeschen
+                finish();
+                System.exit(0);
+            }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
      * assigning values from loyoutViews (Button, textVIew, ImageView)
      */
     protected void initViews() {
-        tv_username = findViewById(R.id.tv_startmenuUsername);
+        tv_username = findViewById(R.id.tv_lobbyUsername);
 
         iv_user_icon = (ImageView) findViewById(R.id.iv_profil);
-        iv_user_icon.setImageResource(R.drawable.ic_user_default);
 
+        ll_profile = findViewById(R.id.ll_profile);
         btn_startGame = findViewById(R.id.btn_start_game);
         btn_getHighscore = findViewById(R.id.btn_getHighscore);
-        btn_logout = findViewById(R.id.btn_logout);
     }
 
     /**
@@ -82,7 +131,7 @@ public class StartmenueActivity extends AppCompatActivity {
      */
     protected void setViews() {
         tv_username.setText(userName);
-        iv_user_icon.setImageResource(R.drawable.ic_user_default);
+        iv_user_icon.setImageResource(R.drawable.ic_profile_w);
     }
 
     /**
@@ -94,9 +143,12 @@ public class StartmenueActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), HighscoreActivity.class);
         startActivity(intent);
     }
-
+    public void getProfile() {
+        Toast.makeText(LobbyActivity.this, "Clicked on Profile", Toast.LENGTH_LONG).show();
+        //startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+    }
     /**
-     * Called when the user taps the Startmenu button
+     * Called when the user taps the Start Game button
      */
     public void startGame(View view) {
         Intent intent = new Intent(getApplicationContext(), InGameActivity.class);
@@ -104,17 +156,18 @@ public class StartmenueActivity extends AppCompatActivity {
     }
 
     /**
-     * Logs user out from server
+     * deletes jwt and user from storage, so tha the user has to log in again to play
      */
-    public void logout(View view) {
-        //TODO: delete user from sharedPref, not from RestSeviceSinglton Instance
-/*        RestServiceSingleton.getInstance(getApplicationContext()).getUser().setToken("");
+    public void logout() {
+
+        sessionManager.logout();
+        Toast.makeText(LobbyActivity.this, "Successfully logged out.", Toast.LENGTH_SHORT).show();
+
+        /* RestServiceSingleton.getInstance(getApplicationContext()).getUser().setToken("");
         Log.i("Token", RestServiceSingleton.getInstance(getApplicationContext()).getUser().getToken());
         RestServiceSingleton.getInstance(getApplicationContext()).setUser(null);
         if (RestServiceSingleton.getInstance(getApplicationContext()).getUser() != null) {
             Log.i("nicht null", "User");
         }*/
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
     }
 }
